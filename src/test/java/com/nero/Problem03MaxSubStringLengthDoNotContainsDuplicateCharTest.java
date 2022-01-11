ffa23f578875636f95087aa707ed3c333af4c2f2
neroHua <38644203+neroHua@users.noreply.github.com>
2022-01-11 13:27:11 +0800

    Create Problem03MaxSubStringLengthDoNotContainsDuplicateCharTest.java

---
+++ src/test/java/com/nero/Problem03MaxSubStringLengthDoNotContainsDuplicateCharTest.java
@@ -0,0 +1,31 @@
+package com.nero;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Problem03MaxSubStringLengthDoNotContainsDuplicateCharTest {
+
+    Problem03MaxSubStringLengthDoNotContainsDuplicateChar tobeTest = new Problem03MaxSubStringLengthDoNotContainsDuplicateChar();
+
+    @Test
+    public void test01() {
+       String s = "pwwkew";
+        int i = tobeTest.lengthOfLongestSubstring(s);
+        Assert.assertEquals(i, 3);
+    }
+
+    @Test
+    public void test02() {
+        String s = "aab";
+        int i = tobeTest.lengthOfLongestSubstring(s);
+        Assert.assertEquals(i, 2);
+    }
+
+    @Test
+    public void test03() {
+        String s = "tmmzuxt";
+        int i = tobeTest.lengthOfLongestSubstring(s);
+        Assert.assertEquals(i, 5);
+    }
+
+}

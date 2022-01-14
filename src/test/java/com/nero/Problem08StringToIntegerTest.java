6ccee3e57d27cc2ca300fcc4104c0fbf626f8506
neroHua <38644203+neroHua@users.noreply.github.com>
2022-01-14 19:14:42 +0800

    Create Problem08StringToIntegerTest.java

---
+++ src/test/java/com/nero/Problem08StringToIntegerTest.java
@@ -0,0 +1,39 @@
+package com.nero;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Problem08StringToIntegerTest {
+
+    Problem08StringToInteger toBeTest = new Problem08StringToInteger();
+
+    @Test
+    public void test01() {
+        Assert.assertEquals(42, toBeTest.myAtoi("42"));
+    }
+
+    @Test
+    public void test02() {
+        Assert.assertEquals(-42, toBeTest.myAtoi("-42"));
+    }
+
+    @Test
+    public void test03() {
+        Assert.assertEquals(0, toBeTest.myAtoi("words and 987"));
+    }
+
+    @Test
+    public void test04() {
+        Assert.assertEquals(0, toBeTest.myAtoi("+-12"));
+    }
+
+    @Test
+    public void test05() {
+        Assert.assertEquals(0, toBeTest.myAtoi(" "));
+    }
+
+    @Test
+    public void test06() {
+        Assert.assertEquals(2147483647, toBeTest.myAtoi("2147483648"));
+    }
+}

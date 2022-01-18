aa3328f8b4201842045585079250dcbe40ddf59f
neroHua <38644203+neroHua@users.noreply.github.com>
2022-01-18 14:57:30 +0800

    Update Problem11ContainerWithMostWater.java

--- src/main/java/com/nero/normal/section01/Problem11ContainerWithMostWater.java
+++ src/main/java/com/nero/normal/section01/Problem11ContainerWithMostWater.java
@@ -33,4 +33,56 @@ public class Problem11ContainerWithMostWater {
         return maxArea;
     }
 
+    /**
+     *  0,1,2,3,4,5,6,7,8
+     * [1,8,6,2,5,4,8,3,7]
+     *
+     * 0 [1][0] max(0) = 1 * max((8 - 0))           = 8
+     * 1 [2][3] max(1) = 2 * max((7 - 3), (3 - 1)   = 8
+     * 2 [3][7] max(2) = 3 * max((8 - 7) || (7 - 1))= 18
+     * 3 [4][5] max(3) = 4 * max((8 - 5) || (5 - 1))= 16
+     * 4 [5][4] max(4) = 5 * max((8 - 4) || (4 - 1))= 20
+     * 5 [6][2] max(5) = 6 * max((8 - 2) || (2 - 1))= 36
+     * 6 [7][8] max(6) = 7 * max((8 - 1))           = 49
+     * 7 [8][1] max(7) = 8 * max((6 - 1))           = 40
+     * 8 [8][6] max(8) = 8 * max((6 - 1))           = 40
+     *
+     * @param height
+     * @return
+     */
+    public int maxArea02(int[] height) {
+        int maxArea = 0;
+        int area = 0;
+        for (int i = 0; i < height.length; i++) {
+            int maxIndex = findHigherMaxIndex(height, i);
+            int minIndex = findHigherMinIndex(height, i);
+            int max = maxIndex - i;
+            int min = i - minIndex;
+            area = height[i] * (max > min ? max : min);
+            maxArea = maxArea > area ? maxArea : area;
+        }
+
+        return maxArea;
+    }
+
+    private int findHigherMaxIndex(int[] height, int i) {
+        for (int j = height.length - 1; j > i; j--) {
+            if (height[j] > height[i]) {
+                return j;
+            }
+        }
+
+        return i;
+    }
+
+    private int findHigherMinIndex(int[] height, int i) {
+        for (int j = 0; j < i; j++) {
+            if (height[j] > height[i]) {
+                return j;
+            }
+        }
+
+        return i;
+    }
+
 }

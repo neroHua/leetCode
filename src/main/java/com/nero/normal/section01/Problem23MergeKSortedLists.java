674e0cbbe35389439e8d4548981cd9d05ca723ba
neroHua <38644203+neroHua@users.noreply.github.com>
2022-01-19 15:26:51 +0800

    Update Problem23MergeKSortedLists.java

--- src/main/java/com/nero/normal/section01/Problem23MergeKSortedLists.java
+++ src/main/java/com/nero/normal/section01/Problem23MergeKSortedLists.java
@@ -1,5 +1,17 @@
 package com.nero.normal.section01;
 
+/**
+ * 给你一个链表数组，每个链表都已经按升序排列。
+ * 请你将所有链表合并到一个升序链表中，返回合并后的链表。
+ *
+ * k == lists.length
+ * 0 <= k <= 10^4
+ * 0 <= lists[i].length <= 500
+ * -10^4 <= lists[i][j] <= 10^4
+ * lists[i] 按 升序 排列
+ * lists[i].length 的总和不超过 10^4
+ *
+ */
 public class Problem23MergeKSortedLists {
 
     class ListNode {

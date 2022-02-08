d8bf38149ddce1f753062932c8b30ac224c94017
neroHua <38644203+neroHua@users.noreply.github.com>
2022-02-08 18:12:23 +0800

    Create Problem41FirstMissingPositive.java

---
+++ src/main/java/com/nero/normal/section01/Problem41FirstMissingPositive.java
@@ -0,0 +1,21 @@
+package com.nero.normal.section01;
+
+/**
+ * 给你一个未排序的整数数组 nums ，请你找出其中没有出现的最小的正整数。
+ * 请你实现时间复杂度为 O(n) 并且只使用常数级别额外空间的解决方案。
+ *
+ * 1 <= nums.length <= 5 * 105
+ * -231 <= nums[i] <= 231 - 1
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 5 * 10 ^ 5
+ * -2 ^ 31 <= nums[i] <= 2 ^ 31 - 1
+ *
+ * 滑德友：
+ * 这个题目我能想到
+ * 1. 快排 + 一轮 但是效率是O(nlog(n))
+ * 2. 利用hash 但是需要额外内存O(n)效率是O(n),这种办法不知道能不能重复利用现在数组，感觉不太可能
+ */
+public class Problem41FirstMissingPositive {
+}

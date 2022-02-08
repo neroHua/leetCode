5ac27709d84758e03034ac47a736defd3bf2f3d7
neroHua <38644203+neroHua@users.noreply.github.com>
2022-02-08 18:17:43 +0800

    Create Problem42TrappingRainWater.java

---
+++ src/main/java/com/nero/normal/section01/Problem42TrappingRainWater.java
@@ -0,0 +1,19 @@
+package com.nero.normal.section01;
+
+/**
+ * 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+ *
+ * 输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+ * 输出：6
+ *
+ * 提示：
+ *
+ * n == height.length
+ * 1 <= n <= 2 * 104
+ * 0 <= height[i] <= 105
+ *
+ * 滑德友：
+ * 看着跟之前最长有效小括号的情况有点类似，直觉上有个O(n)办法
+ */
+public class Problem42TrappingRainWater {
+}

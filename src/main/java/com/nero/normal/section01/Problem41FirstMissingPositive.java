5567d9647bea0676226a1d7d6b781fdefac1ea87
neroHua <38644203+neroHua@users.noreply.github.com>
2022-02-10 14:35:22 +0800

    Update Problem41FirstMissingPositive.java

--- src/main/java/com/nero/normal/section01/Problem41FirstMissingPositive.java
+++ src/main/java/com/nero/normal/section01/Problem41FirstMissingPositive.java
@@ -16,6 +16,7 @@ package com.nero.normal.section01;
  * 这个题目我能想到
  * 1. 快排 + 一轮 但是效率是O(nlog(n))
  * 2. 利用hash 但是需要额外内存O(n)效率是O(n),这种办法不知道能不能重复利用现在数组，感觉不太可能
+ * 3. 利用线性排序中的计数排序思想, 把当前数组当做辅助数组，大于和小于当前数组的长度的数值直接丢弃即可。即是一轮插入，一轮结果2n
  */
 public class Problem41FirstMissingPositive {
 }

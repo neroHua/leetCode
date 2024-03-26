cbb213f47d589259aa772ac55555f2776af88e4b
neroHua <2676337735@qq.com>
2024-03-26 22:01:34 +0800

    add comments for problem 20

--- src/main/java/com/nero/normal/section01/Problem20ValidParentheses.java
+++ src/main/java/com/nero/normal/section01/Problem20ValidParentheses.java
@@ -3,6 +3,20 @@ package com.nero.normal.section01;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+ *
+ * 有效字符串需满足：
+ *
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 每个右括号都有一个对应的相同类型的左括号。
+ *
+ * 提示：
+ *
+ * 1 <= s.length <= 104
+ * s 仅由括号 '()[]{}' 组成
+ */
 public class Problem20ValidParentheses {
 
     /**
